14837874d22c6a69a8677db81a13edc41f62d48d
soragogo <es377233@gmail.com>
2025-03-17 23:01:12 +0900

    返り値もconst refのが良い

--- ex00/main.hpp
+++ ex00/main.hpp
@@ -12,12 +12,12 @@ void swap(T & a, T & b) {
 }
 
 template <typename T>
-T min(T const & a, T const & b) {
+T const & min(T const & a, T const & b) {
     return a < b ? a : b;
 }
 
 template <typename T>
-T max(T const & a, T const & b) {
+T const & max(T const & a, T const & b) {
     return a > b ? a : b;
 }
 
