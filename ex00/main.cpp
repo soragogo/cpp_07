74e0c168aebe92104f99c44bfc383ea6b5b3fde5
soragogo <es377233@gmail.com>
2025-03-17 22:34:24 +0900

    exampleを使う

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,50 +1,68 @@
 #include "main.hpp"
 
-int main() {
-    int a = 1;
-    int b = 2;
-    std::cout << "a: "<< a << " b: " << b << std::endl;
-    std::cout << "min: " << min(a, b) << std::endl;
-    std::cout << "max: " << max(a, b) << std::endl;
-    swap(a, b);
-    std::cout << "a: "<< a << " b: " << b << std::endl;
+// int main() {
+//     int a = 1;
+//     int b = 2;
+//     std::cout << "a: "<< a << " b: " << b << std::endl;
+//     std::cout << "min: " << min(a, b) << std::endl;
+//     std::cout << "max: " << max(a, b) << std::endl;
+//     swap(a, b);
+//     std::cout << "a: "<< a << " b: " << b << std::endl;
 
-    std::cout << "--------------------------------------------------" << std::endl;
-    std::string c = "Hello";
-    std::string d = "World";
-    std::cout << "c: "<< c << " d: " << d << std::endl;
-    std::cout << "min: " << min(c, d) << std::endl;
-    std::cout << "max: " << max(c, d) << std::endl;
-    swap(c, d);
-    std::cout << "c: "<< c << " d: " << d << std::endl;
+//     std::cout << "--------------------------------------------------" << std::endl;
+//     std::string c = "Hello";
+//     std::string d = "World";
+//     std::cout << "c: "<< c << " d: " << d << std::endl;
+//     std::cout << "min: " << min(c, d) << std::endl;
+//     std::cout << "max: " << max(c, d) << std::endl;
+//     swap(c, d);
+//     std::cout << "c: "<< c << " d: " << d << std::endl;
 
 
-    std::cout << "--------------------------------------------------" << std::endl;
-    float e = 1.11111;
-    float f = 2.22222;
-    std::cout << "e: "<< e << " f: " << f << std::endl;
-    std::cout << "min: " << min(e, f) << std::endl;
-    std::cout << "max: " << max(e, f) << std::endl;
-    swap(e, f);
-    std::cout << "e: "<< e << " f: " << f << std::endl;
+//     std::cout << "--------------------------------------------------" << std::endl;
+//     float e = 1.11111;
+//     float f = 2.22222;
+//     std::cout << "e: "<< e << " f: " << f << std::endl;
+//     std::cout << "min: " << min(e, f) << std::endl;
+//     std::cout << "max: " << max(e, f) << std::endl;
+//     swap(e, f);
+//     std::cout << "e: "<< e << " f: " << f << std::endl;
 
-    std::cout << "--------------------------------------------------" << std::endl;
-    std::string g = "Hello";
-    std::string h = "World";
-    std::cout << "g: "<< g << " h: " << h << std::endl;
-    std::cout << "min: " << min(g, h) << std::endl;
-    std::cout << "max: " << max(g, h) << std::endl;
-    swap(g, h);
-    std::cout << "g: "<< g << " h: " << h << std::endl;
+//     std::cout << "--------------------------------------------------" << std::endl;
+//     std::string g = "Hello";
+//     std::string h = "World";
+//     std::cout << "g: "<< g << " h: " << h << std::endl;
+//     std::cout << "min: " << min(g, h) << std::endl;
+//     std::cout << "max: " << max(g, h) << std::endl;
+//     swap(g, h);
+//     std::cout << "g: "<< g << " h: " << h << std::endl;
 
-    std::cout << "--------------------------------------------------" << std::endl;
-    bool i = true;
-    bool j = false;
-    std::cout << "i: "<< i << " j: " << j << std::endl;
-    std::cout << "min: " << min(i, j) << std::endl;
-    std::cout << "max: " << max(i, j) << std::endl;
-    swap(i, j);
-    std::cout << "i: "<< i << " j: " << j << std::endl;
+//     std::cout << "--------------------------------------------------" << std::endl;
+//     bool i = true;
+//     bool j = false;
+//     std::cout << "i: "<< i << " j: " << j << std::endl;
+//     std::cout << "min: " << min(i, j) << std::endl;
+//     std::cout << "max: " << max(i, j) << std::endl;
+//     swap(i, j);
+//     std::cout << "i: "<< i << " j: " << j << std::endl;
 
-    return (0);
+//     return (0);
+// }
+
+
+// example in the pdf:
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
 }
